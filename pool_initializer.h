3c2ba8855ef292bf6f15c61f3e717ebeeb071921
Maksym Bevza <maksymbevza@gmail.com>
2016-01-24 15:08:02 +0200

    Add PoolInitializer and g++11

---
+++ pool_initializer.h
@@ -0,0 +1,23 @@
+#ifndef POOL_INITIALIZER_H
+#define POOL_INITIALIZER_H
+
+using namespace std;
+
+class PoolInitializer {
+  vector<ServerAssign> &servers_;
+
+  PoolInitializer(vector<ServerAssign> &servers) : servers_(servers) {
+  }
+
+  int calculate() {
+    return -1;
+  }
+
+ public:
+  static int calc(vector<ServerAssign> &servers) {
+    PoolInitializer initializer(servers);
+    return initializer.calculate();
+  }
+};
+
+#endif
